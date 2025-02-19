77da8d57ffa19a6330abaadb1a04b745aa86e349
Ian Swart <ianswart13@gmail.com>
2025-02-19 22:16:22 +0200

    Update env

--- app/env.ts
+++ app/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-    numberOfSundays: parseInt(process.env.NUMBER_OF_SUNDAYS || '6'),
-    dateOrigin: new Date(process.env.DATE_ORIGIN || '2025-02-09'),
-    seed: parseInt(process.env.SEED || '1517'),
-    diakens: process.env.DIAKENS ? process.env.DIAKENS.split(',') : ["Petrus","Andreas","Jakobus","Johannes","Filippus","Bartolomeus","Tomas","Matteus","Jakobus","Taddeus","Simon","Judas"]
+    numberOfSundays: parseInt(process.env.NEXT_PUBLIC_NUMBER_OF_SUNDAYS || '6'),
+    dateOrigin: new Date(process.env.NEXT_PUBLIC_DATE_ORIGIN || '2025-02-09'),
+    seed: parseInt(process.env.NEXT_PUBLIC_SEED || '1517'),
+    diakens: process.env.NEXT_PUBLIC_DIAKENS ? process.env.NEXT_PUBLIC_DIAKENS.split(',') : ["Petrus","Andreas","Jakobus","Johannes","Filippus","Bartolomeus","Tomas","Matteus","Jakobus","Taddeus","Simon","Judas"]
 };
